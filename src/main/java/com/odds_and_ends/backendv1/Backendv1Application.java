65acfe6ac994af70b4bf4ccb941da987f5f95886
siwony_ <siwon103305@gmail.com>
2021-11-23 22:49:44 +0900

    Init 프로젝트 초기화

---
+++ src/main/java/com/odds_and_ends/backendv1/Backendv1Application.java
@@ -0,0 +1,13 @@
+package com.odds_and_ends.backendv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backendv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backendv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/odds_and_ends/backendv1/Backendv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odds_and_ends.backendv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backendv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
