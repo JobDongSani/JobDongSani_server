b00fe3a853808479e9ec5a46d58a2e5ec48fa342
siwony_ <siwon103305@gmail.com>
2021-11-23 23:18:44 +0900

    Add swagger

---
+++ src/main/java/com/odds_and_ends/backendv1/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.odds_and_ends.backendv1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.odds_and_ends.backendv1"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("잡동사니 API DOC")
+                .description("잡동사니 API DOC")
+                .version("1.0")
+                .build();
+    }
+}
