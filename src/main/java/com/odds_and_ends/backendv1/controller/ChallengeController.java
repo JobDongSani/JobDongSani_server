57ae02c39260bb16128288bbd68466a7290db361
jhhong0509 <jhhong0509@naver.com>
2021-11-24 21:52:15 +0900

    [UPDATE] saveChallenge 컨트롤러 추가

--- src/main/java/com/odds_and_ends/backendv1/controller/ChallengeController.java
+++ src/main/java/com/odds_and_ends/backendv1/controller/ChallengeController.java
@@ -1,13 +1,11 @@
 package com.odds_and_ends.backendv1.controller;
 
+import com.odds_and_ends.backendv1.payload.request.ChallengeRequest;
 import com.odds_and_ends.backendv1.payload.response.ChallengeDetailResponse;
 import com.odds_and_ends.backendv1.payload.response.CommonResponse;
 import com.odds_and_ends.backendv1.service.ChallengeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,4 +18,11 @@ public class ChallengeController {
     public CommonResponse<ChallengeDetailResponse> getChallengeDetail(@PathVariable Long id) {
         return challengeService.getDetailResponse(id);
     }
+
+    @PostMapping
+    public CommonResponse<String> saveChallenge(@RequestBody ChallengeRequest request) {
+        return challengeService.saveChallenge(request);
+    }
+
+
 }
