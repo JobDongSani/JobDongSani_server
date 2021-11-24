0110d3ea4f83fdcf04d5a69e248c0ac20bdee905
siwony_ <siwon103305@gmail.com>
2021-11-25 08:37:09 +0900

    Update TrashShareBoard getter 추가'

--- src/main/java/com/odds_and_ends/backendv1/entity/trash_share_board/TrashShareBoard.java
+++ src/main/java/com/odds_and_ends/backendv1/entity/trash_share_board/TrashShareBoard.java
@@ -3,12 +3,13 @@ package com.odds_and_ends.backendv1.entity.trash_share_board;
 import com.odds_and_ends.backendv1.entity.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity @Table(name = "trash_share_board")
-@Builder
+@Builder @Getter
 @NoArgsConstructor @AllArgsConstructor
 public class TrashShareBoard {
 
