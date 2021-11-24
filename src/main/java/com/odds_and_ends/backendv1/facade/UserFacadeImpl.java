2f13472cea722dcfa4d3a552bc28c8b5fd2ec6cd
jhhong0509 <jhhong0509@naver.com>
2021-11-24 14:22:28 +0900

    [ADD] UserFacade

---
+++ src/main/java/com/odds_and_ends/backendv1/facade/UserFacadeImpl.java
@@ -0,0 +1,35 @@
+package com.odds_and_ends.backendv1.facade;
+
+import com.odds_and_ends.backendv1.entity.user.User;
+import com.odds_and_ends.backendv1.entity.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class UserFacadeImpl implements UserFacade {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User getCurrentUser() {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        if(name == null) {
+            throw new RuntimeException();
+        }
+        return getUserById(Long.valueOf(name));
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(RuntimeException::new);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(RuntimeException::new);
+    }
+}
