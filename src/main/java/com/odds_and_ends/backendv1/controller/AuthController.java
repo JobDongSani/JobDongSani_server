a7093b1398ce9f1feafe7e661703586048598d6b
jhhong0509 <jhhong0509@naver.com>
2021-11-24 14:16:07 +0900

    [ADD] 로그인 추가

---
+++ src/main/java/com/odds_and_ends/backendv1/controller/AuthController.java
@@ -0,0 +1,14 @@
+package com.odds_and_ends.backendv1.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/")
+public class AuthController {
+
+    @PostMapping("")
+}
