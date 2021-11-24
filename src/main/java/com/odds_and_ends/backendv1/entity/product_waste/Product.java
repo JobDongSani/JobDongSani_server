49ec3bb937fa70457021c5d8db4ad565f7e31458
siwony_ <siwon103305@gmail.com>
2021-11-24 21:39:36 +0900

    Add 상품의 바코드와 폐기물 종류를 저장하는 Entity 생성 + Repository 생성

---
+++ src/main/java/com/odds_and_ends/backendv1/entity/product_waste/Product.java
@@ -0,0 +1,23 @@
+package com.odds_and_ends.backendv1.entity.product_waste;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Table(name = "product_waste") @Entity
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Builder
+public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "waste_type")
+    private String waste_type;
+
+    @Column(name = "product_barcode")
+    private String barcode;
+}
---
+++ src/main/java/com/odds_and_ends/backendv1/entity/product_waste/ProductRepository.java
@@ -0,0 +1,6 @@
+package com.odds_and_ends.backendv1.entity.product_waste;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
