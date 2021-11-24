ea9df51ae29427967c031bde5cf8dc9b2c0f7527
siwony_ <siwon103305@gmail.com>
2021-11-24 23:49:28 +0900

    Add TrashShareBoardRepository

---
+++ src/main/java/com/odds_and_ends/backendv1/entity/trash_share_board/TrashShareBoardRepository.java
@@ -0,0 +1,7 @@
+package com.odds_and_ends.backendv1.entity.trash_share_board;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrashShareBoardRepository extends JpaRepository<TrashShareBoard, Long> {
+
+}
