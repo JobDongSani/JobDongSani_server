9b1995ba8c12903e42a27f594c2af61e95ea2cc1
jhhong0509 <jhhong0509@naver.com>
2021-11-24 23:37:11 +0900

    [FIX] UserService에서 PasswordEncoder 추가

--- src/main/java/com/odds_and_ends/backendv1/service/UserService.java
+++ src/main/java/com/odds_and_ends/backendv1/service/UserService.java
@@ -6,6 +6,7 @@ import com.odds_and_ends.backendv1.exceptions.ExistUsernameException;
 import com.odds_and_ends.backendv1.payload.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -16,11 +17,13 @@ import java.io.IOException;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public CommonResponse<String> signup(SignupVo signupVo) throws IOException {
         if (userRepository.existsByUsername(signupVo.getUsername()))
             throw new ExistUsernameException();
 
+        signupVo.setPassword(passwordEncoder.encode(signupVo.getPassword()));
         userRepository.save(signupVo.toEntity());
 
         return new CommonResponse<>(200, "회원가입 성공", "data");
