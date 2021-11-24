b286c6c20f1dbacdc9c765230d3c48f325fe92c1
jhhong0509 <jhhong0509@naver.com>
2021-11-24 16:25:54 +0900

    [FIX] ExceptionFilter 오류 수조ㅓㅇ

--- src/main/java/com/odds_and_ends/backendv1/config/exception/ExceptionFilter.java
+++ src/main/java/com/odds_and_ends/backendv1/config/exception/ExceptionFilter.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.NestedServletException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -21,6 +22,13 @@ public class ExceptionFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
             filterChain.doFilter(request, response);
+        } catch (NestedServletException e) {
+            Throwable throwable = e.getCause();
+            if (throwable instanceof GlobalException exception) {
+                sendExpectedExceptionMessage(response, exception);
+            } else {
+                sendUnexpectedExceptionMessage(response, 500, throwable.getMessage());
+            }
         } catch (GlobalException exception) {
             sendExpectedExceptionMessage(response, exception);
         } catch (Exception exception) {
