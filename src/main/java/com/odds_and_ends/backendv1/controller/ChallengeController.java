2f1330bcd7c0c0b6034f7075c51483d4f75c48d7
jhhong0509 <jhhong0509@naver.com>
2021-11-24 22:03:10 +0900

    [UPDATE] Controller에 추가

--- src/main/java/com/odds_and_ends/backendv1/controller/ChallengeController.java
+++ src/main/java/com/odds_and_ends/backendv1/controller/ChallengeController.java
@@ -2,9 +2,11 @@ package com.odds_and_ends.backendv1.controller;
 
 import com.odds_and_ends.backendv1.payload.request.ChallengeRequest;
 import com.odds_and_ends.backendv1.payload.response.ChallengeDetailResponse;
+import com.odds_and_ends.backendv1.payload.response.ChallengeListResponse;
 import com.odds_and_ends.backendv1.payload.response.CommonResponse;
 import com.odds_and_ends.backendv1.service.ChallengeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,6 +19,17 @@ public class ChallengeController {
     @GetMapping("/{id}")
     public CommonResponse<ChallengeDetailResponse> getChallengeDetail(@PathVariable Long id) {
         return challengeService.getDetailResponse(id);
+    }
+
+    @GetMapping
+    public CommonResponse<ChallengeListResponse> getChallengeList(Pageable pageable) {
+        return challengeService.getChallengeList(pageable);
+    }
+
+    @GetMapping("/filter")
+    public CommonResponse<ChallengeListResponse> findChallengeByTitle(@RequestParam String title,
+                                                                        Pageable pageable) {
+        return challengeService.findChallengesByTitle(title, pageable);
     }
 
     @PostMapping
