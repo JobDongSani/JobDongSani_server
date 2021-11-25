5f522a315c5a423a7219b356ff9a9f311ce31747
siwony_ <siwon103305@gmail.com>
2021-11-25 09:21:45 +0900

    Update 쓰래기 나눔 개시판 API완료

---
+++ src/main/java/com/odds_and_ends/backendv1/controller/TrashShareBoardController.java
@@ -0,0 +1,42 @@
+package com.odds_and_ends.backendv1.controller;
+
+import com.odds_and_ends.backendv1.dto.trash_share_board.TrashShareBoardDto;
+import com.odds_and_ends.backendv1.payload.response.CommonResponse;
+import com.odds_and_ends.backendv1.service.TrashShareBoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/trash-share-board")
+public class TrashShareBoardController {
+
+    private final TrashShareBoardService trashShareBoardService;
+
+    @PostMapping
+    private CommonResponse<Long> save(@RequestBody TrashShareBoardDto trashShareBoardDto){
+        return trashShareBoardService.save(trashShareBoardDto);
+    }
+
+    @GetMapping("/{id}")
+    private CommonResponse<TrashShareBoardDto> findById(@PathVariable("id"), long id){
+        return trashShareBoardService.findById(id);
+    }
+
+    @GetMapping("/")
+    private CommonResponse<List<TrashShareBoardDto>> findAll(){
+        return trashShareBoardService.findAll();
+    }
+
+    @PutMapping("/{id}")
+    private CommonResponse<Long> update(@PathVariable long id, @RequestBody TrashShareBoardDto trashShareBoardDto){
+        return trashShareBoardService.update(id, trashShareBoardDto);
+    }
+
+    @DeleteMapping("{id}")
+    private CommonResponse<Void> delete(@PathVariable("id") long id){
+        return trashShareBoardService.delete(id);
+    }
+}
