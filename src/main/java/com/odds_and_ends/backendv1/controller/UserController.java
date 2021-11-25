79e813ac6dcaad52c6584624b2113e452d7d0e8d
siwony_ <siwon103305@gmail.com>
2021-11-25 14:34:06 +0900

    Update 유저 프로필 로직 완료

--- src/main/java/com/odds_and_ends/backendv1/controller/UserController.java
+++ src/main/java/com/odds_and_ends/backendv1/controller/UserController.java
@@ -3,6 +3,7 @@ package com.odds_and_ends.backendv1.controller;
 import com.odds_and_ends.backendv1.dto.user.UserDto;
 import com.odds_and_ends.backendv1.payload.response.CommonResponse;
 import com.odds_and_ends.backendv1.service.UserService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,5 +18,10 @@ public class UserController {
     @PostMapping("/signup")
     private CommonResponse<String> signup(@RequestBody UserDto userDto) throws IOException {
         return userService.signup(userDto);
+    }
+
+    @GetMapping("/profile")
+    private CommonResponse<UserDto> showProfile() throws IOException {
+        return userService.showProfile();
     }
 }
